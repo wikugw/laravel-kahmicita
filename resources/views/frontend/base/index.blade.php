204478c733099e1b6a07fc6bb54b2fde0679e05f
Wiku Galindra Wardhana <79816547+Wiku-Grune@users.noreply.github.com>
2021-04-29 20:30:05 +0700

    setup laravel tambah halaman home

---
+++ resources/views/frontend/base/index.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @yield('title')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="assets/img/apple-icon.png">
+    <link rel="shortcut icon" type="image/x-icon" style="border-radius: 10px !important;" href="{{ asset('frontend/img/favicon.jpeg') }}">
+    <!-- Load Require CSS -->
+    <link href="{{asset ('frontend/css/bootstrap.css') }}" rel="stylesheet">
+    <!-- Font CSS -->
+    <link href="{{asset ('frontend/css/boxicon.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+    <!-- Load Tempalte CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/templatemo.css') }}">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
+<!--
+    
+TemplateMo 561 Purple Buzz
+
+https://templatemo.com/tm-561-purple-buzz
+
+-->
+</head>
+
+<body>
+    <!-- Header -->
+    <nav id="main_nav" class="navbar navbar-expand-lg navbar-light bg-white shadow" style="background-color: #FAD586 !important;">
+        <div class="container d-flex justify-content-between align-items-center" >
+            <a class="navbar-brand h1" href="index.html">
+                <i class=''>
+                    <img src="{{ asset('frontend/img/WhatsApp Image 2021-04-23 at 5.16.06 PM.png') }}" height="40"  alt="">
+                </i>
+                <span class="text-dark h4">Kahmi</span> <span class="text-primary h4">Cita</span>
+            </a>
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggler-success" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="navbar-toggler-success">
+                <div class="flex-fill mx-xl-5 mb-2">
+                    <ul class="nav navbar-nav d-flex justify-content-between mx-xl-5 text-center text-dark">
+                        <li class="nav-item">
+                            <a class="nav-link btn-outline-primary rounded-pill px-3" style="font-weight: bold; font-size: 18px;" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- Example single danger button -->
+                            <div class="dropdown">
+                                <button 
+                                    class="nav-link btn-outline-transparent rounded-pill px-3 dropdown-toggle" style="font-weight: bold; font-size: 18px;" 
+                                    type="button" id="dropdownMenuButton" data-toggle="dropdown" 
+                                    aria-haspopup="true" aria-expanded="false"
+                                    style="outline: none;"
+                                >
+                                  Profil
+                                </button>
+                                <div class="dropdown-menu" id="dropdown-nav" aria-labelledby="dropdownMenuButton">
+                                  <a class="dropdown-item" href="#maksudTujuan">Maksud dan Tujuan</a>
+                                  <a class="dropdown-item" href="./akta.html">Akta Yayasan Kahmi</a>
+                                  <a class="dropdown-item" href="#">Struktur Organisasi</a>
+                                </div>
+                              </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link btn-outline-primary rounded-pill px-3" style="font-weight: bold; font-size: 18px;" href="#infoKegiatan">Info kegiatan</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="navbar align-self-center d-flex justify-self-center">
+                <a class="nav-link" href="#"><i class='bx bx-user-circle bx-sm text-primary'></i></a>
+            </div>
+        </div>
+    </nav>
+    <!-- Close Header -->
+
+    @yield('content')
+
+    <!-- Start Footer -->
+    <footer class="bg-secondary pt-4">
+        <div class="container">
+            <div class="row py-4">
+
+                <div class="col-lg-5 col-12 align-left">
+                    <a class="navbar-brand" href="index.html">
+                        <i class=''>
+                            <img src="{{ asset('frontend/img/WhatsApp Image 2021-04-23 at 5.16.06 PM.png') }}" height="40"  alt="">
+                        </i>
+                        <span class="text-light h5">Kahmi</span> <span class="text-light h5 semi-bold-600">Cita</span>
+                    </a>
+                    <p class="text-light my-lg-4 my-2">
+                        Perum Puri Nirwana Gajayana Kav 40 Jl. Simpang Gajayana Merjosari, Dinoyo, Kec. Lowokwaru, Kota Malang, Jawa Timur 65144
+                    </p>
+                    <ul class="list-inline footer-icons light-300">
+                        <li class="list-inline-item m-0">
+                            <a class="text-light" target="_blank" href="http://facebook.com/">
+                                <i class='bx bxl-facebook-square bx-md'></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item m-0">
+                            <a class="text-light" target="_blank" href="https://www.whatsapp.com/">
+                                <i class='bx bxl-whatsapp-square bx-md'></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-4 my-sm-0 mt-4">
+                    <h3 class="h4 pb-lg-3 text-light light-300">Tautan Lainnya</h2>
+                        <ul class="list-unstyled text-light light-300">
+                            <li class="pb-2">
+                                <i class='bx-fw bx bxs-chevron-right bx-xs'></i><a class="text-decoration-none text-light" href="index.html">Home</a>
+                            </li>
+                            <li class="pb-2">
+                                <i class='bx-fw bx bxs-chevron-right bx-xs'></i><a class="text-decoration-none text-light py-1" href="about.html">Mukadimah</a>
+                            </li>
+                            <li class="pb-2">
+                                <i class='bx-fw bx bxs-chevron-right bx-xs'></i><a class="text-decoration-none text-light py-1" href="work.html">Maksud dan Tujuan</a>
+                            </li>
+                            <li class="pb-2">
+                                <i class='bx-fw bx bxs-chevron-right bx-xs'></i></i><a class="text-decoration-none text-light py-1" href="pricing.html">Info Kegiatan</a>
+                            </li>
+                        </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-4 my-sm-0 mt-4">
+                    <h2 class="h4 pb-lg-3 text-light light-300">Kontak Kami</h2>
+                    <ul class="list-unstyled text-light light-300">
+                        <li class="pb-2">
+                            <i class='bx-fw bx bx-phone bx-xs'></i><a class="text-decoration-none text-light py-1" href="tel:010-020-0340">010-020-0340</a>
+                        </li>
+                        <li class="pb-2">
+                            <i class='bx-fw bx bx-mail-send bx-xs'></i><a class="text-decoration-none text-light py-1" href="mailto:info@company.com">info@company.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+
+    </footer>
+    <!-- End Footer -->
+
+
+    <!-- Bootstrap -->
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Load jQuery require for isotope -->
+    <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
+    <!-- Isotope -->
+    <script src="{{ asset('frontend/js/isotope.pkgd.js') }}"></script>
+    <!-- Page Script -->
+    <script>
+        $(window).load(function() {
+
+            // trigger nav dropdown button
+            $('.dropdown-toggle').click(function() {
+                if ($('#dropdown-nav').hasClass("show")){
+                    $( '#dropdown-nav' ).removeClass('show');
+                } else {
+                    $( '#dropdown-nav' ).addClass('show');
+                }
+            });
+
+            // hide dropdown button
+            $(document).on("click", function(e) {
+                if ($(e.target).is(".dropdown-toggle") === false) {
+                    $( '#dropdown-nav' ).removeClass('show');
+                }
+            });
+
+            // init Isotope
+            var $projects = $('.projects').isotope({
+                itemSelector: '.project',
+                layoutMode: 'fitRows'
+            });
+            $(".filter-btn").click(function() {
+                var data_filter = $(this).attr("data-filter");
+                $projects.isotope({
+                    filter: data_filter
+                });
+                $(".filter-btn").removeClass("active");
+                $(".filter-btn").removeClass("shadow");
+                $(this).addClass("active");
+                $(this).addClass("shadow");
+                return false;
+            });
+        });
+    </script>
+    <!-- Templatemo -->
+    <script src="{{ asset('frontend/js/templatemo.js')}}"></script>
+    <!-- Custom -->
+    <script src="{{ asset('frontend/js/custom.js')}}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/frontend/home/index.blade.php
@@ -0,0 +1,174 @@
+@extends('frontend.base.index')
+
+@section('title')
+<title>Home | Yayasan KAHMI Cita</title>
+@endsection
+
+@section('content')
+
+<!-- Start Banner Hero -->
+<div id="work_banner" class="banner-wrapper bg-light w-100 py-5" style="height: 500px;">
+    <div class="banner-vertical-center-work container text-light d-flex justify-content-center align-items-center py-5 p-0">
+        <div class="banner-content col-lg-8 col-12 m-lg-auto text-center">
+            <h1 class="banner-heading h2 display-3 pb-5 semi-bold-600"></h1>
+            <h3 class="h4 pb-2 regular-400"></h3>
+            <p class="banner-body pb-2 light-300"></p>
+        </div>
+    </div>
+</div>
+<!-- End Banner Hero -->
+
+
+
+<!-- Start Service -->
+<section class="service-wrapper py-3">
+    <div class="container-fluid pb-3">
+        <div class="row mt-4">
+            <div class="service-header col-2 col-lg-3 text-end light-300">
+                <i class='bx bx-moon h3 mt-1'></i>
+            </div>
+            <div class="service-heading col-10 col-lg-9 text-start float-end light-300">
+                <h2 class="h3 pb-4 typo-space-line">Mukadimah</h2>
+            </div>
+        </div>
+        <p class="service-footer col-10 offset-2 col-lg-9 offset-lg-3 text-start pb-3 text-muted px-2">
+            Bismillahirrahmanirrahim
+            <br>
+            <br>
+            Bahwa kebenaran hakiki bersumber dari  firman Allah SWT  dan  Sabda Nabi Muhammad SAW  yang terangkum dalam ajaran Al-Islam sebagai agama yang sempurna, sebagai pedoman hidup dalam meraih keselamatan di dunia dan di akhirat, manusia diperintahkan untuk menyempurnakan ikhtiar secara makruf dalam hubungannya dengan sesama manusia, alam semesta, bertindak adil,  mencegah perbuatan tercela serta saling menasehati dalam kebaikan dan taqwa sebagai wujud kesadaran keimanan, keindonesiaan dan kemanusiaan yang gerak langkahnya dilandasi keyakinan sempurna tiada Tuhan selain Allah dan Nabi Muhammad adalah utusan Allah.
+            <br>
+            <br>
+            Sejarah mencatat peran strategis Umat Islam yang menjadi penggerak, pembaharu dan pejuang dalam merebut dan mempertahankan kemerdekaan Negara Republik Indonesia sesuai amanah konstitusi yakni melindungi segenap bangsa dan tumpah darah Indonesia, dan ikut melaksanakan ketertiban dunia berdasarkan kemerdekaan, perdamaian abadi, dan keadilan sosial.
+            <br>
+            <br>
+            Menghimpun potensi dan kekuatan kader bangsa sebagai bagian yang tak terpisahkan dari Masyarakat  Indonesia untuk  secara aktif berkontribusi  dalam membangun masyarakat yang adil dan makmur yang di ridhoi Allah SWT dalam Negara kesatuan Republik Indonesia merupakan tugas mulia. Hal tersebut dilaksanakan dengan penuh tanggung jawab melalui niat yang ikhlas, kerja keras dan cerdas, dilandasi kesadaran spiritual akan kepasrahan total hanya kepada Allah Yang Maha Pengatur.
+        </p>
+    </div>
+
+</section>
+<!-- End Service -->
+
+<section>
+    <div class="container my-5">
+        <div class="recent-work-header row pb-3 text-center">
+            <h2 class="col-md-6 m-auto h2 semi-bold-600 pt-2 typo-space-line-center-2" style="color: #184d47 ; font-size: 28px;">Tentang Kami</h2>
+        </div>
+        <div class="row py-5">
+            <div class="col-12 col-md-6">
+                <p class="feature-work-body text-muted light-300 pt-2" style=" font-weight: bolder; font-size: 16px;">
+                    Perkumpulan ini bernama “Keluarga Al Hikmah Madani Insancita” di singkat dengan “KAHMI Cita” (untuk selanjutnya cukup disebut “Yayasan”). Yayasan ini bertempat kedudukan di Kota Malang, Provinsi Jawa Timur, Indonesia. Yayasan KAHMI Cita didirikan dengan maksud mewujudkan masyarakat Makmur, Adil, Damai dan Aman bernafaskan Islam (MADANI) dalam bidang sosial keagamaan.
+                </p>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="row justify-content-center">
+                    <iframe width="420" height="315" src="https://www.youtube.com/embed/pH64GZIjYjA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Start Feature Work -->
+<section class="bg-light py-5" id="maksudTujuan" style="background-color: #e8eae6 !important;">
+    <div class="feature-work container my-4">
+        <div class="recent-work-header row pb-3 text-center">
+            <h2 class="col-md-6 m-auto h2 semi-bold-600 pb-5 typo-space-line-center-2" style="color: #184d47; font-size: 28px;">Maksud & Tujuan</h2>
+        </div>
+        <div class="row d-flex align-items-stretch justify-content-between">
+            <div class="col-lg-6 px-2 py-4 w-48 my-1" style="background-color: #184d47; border-radius: 10px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); border-color: #fff !important; border-radius: 10px; border: solid #fff; border-width: 10px;">
+                <div class="p-5 my-5">
+                    <h1 class="feature-work-heading h3 py-3 semi-bold-600 typo-space-line" style="color: #FAD586;">Maksud</h1>
+                    <p class="feature-work-body text-muted light-300 pt-2" style="color: #fff !important; font-weight: bolder; font-size: 20px;">
+                        Yayasan KAHMI CITA didirikan dengan maksud mewujudkan masyarakat Makmur, Adil, Damai dan Aman bernafaskan Islam (MADANI) dalam bidang sosial keagamaan.
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-6 py-4 my-1" style="padding-left:56px !important; padding-right: 56px !important; background-color: #184d47; border-radius: 10px; border: solid #fff; border-width: 10px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);">
+                <div class="pl-2">
+                    <h1 class="feature-work-heading h3 py-3 semi-bold-600 typo-space-line" style="color: #FAD586;">Tujuan</h1>
+                    <p class="feature-work-body text-muted light-300 pt-2" style="font-weight: bold; color: #fff !important">
+                        Tujuan pendirian Yayasan KAHMI CITA meliputi:
+                        <br><br>
+                        <strong>A.</strong>	 Terwujudnya kualitas insan yang bertaqwa, berilmu, berkarakter, beretika, berbudaya, dan berkinerja tinggi dalam mentransformasikan kehidupan umat ke arah yang lebih baik sehingga tercipta kemakmuran umat.
+                        <br><br>
+                        <strong>B.</strong>	Terwujudnya kualitas insan yang peduli dan humanis berjiwa melayani dan mengabdi untuk kesejahteraan dan kebahagiaan umat sehingga tercipta keadilan bagi umat manusia.
+                        <br><br>
+                        <strong>C.</strong>	Terwujudnya kualitas insan yang kreatif, inovatif, produktif, adaptif, dan komunikatif dalam era kemajuan perkembangan teknologi dan informasi untuk menjaga perdamaian dan keamanan dunia. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Feature Work -->
+
+<!-- Start Recent Work -->
+<section class="py-3 mb-5" id="infoKegiatan" style="background-color: #C64756;">
+    <div class="container">
+        <div class="recent-work-header row pb-3 text-center">
+            <h2 class="col-md-6 m-auto h2 semi-bold-600 py-5 typo-space-line-center" style="color: #fff;">Info Kegiatan</h2>
+        </div>
+        <div class="row gy-5 g-lg-5 mb-4">
+
+            <!-- Start Recent Work -->
+            <div class="col-md-4 mb-3">
+                <a href="#" class="recent-work card border-0 shadow-lg overflow-hidden">
+                    <img class="recent-work-img card-img" src="{{ asset('frontend/img/kahmicita-open-donasi.jpeg') }}" alt="Card image">
+                    <div class="recent-work-vertical card-img-overlay d-flex align-items-end">
+                        <div class="recent-work-content text-start mb-3 ml-3 text-dark">
+                            <h3 class="card-title light-300">Kahmi Cita</h3>
+                            <p class="card-text">Ullamco laboris nisi ut aliquip ex</p>
+                        </div>
+                    </div>
+                </a>
+            </div><!-- End Recent Work -->
+
+            <!-- Start Recent Work -->
+            <div class="col-md-4 mb-3">
+                <a href="#" class="recent-work card border-0 shadow-lg overflow-hidden">
+                    <img class="recent-work-img card-img" src="{{ asset('frontend/img/kahmicita-open-donasi.jpeg') }}" alt="Card image">
+                    <div class="recent-work-vertical card-img-overlay d-flex align-items-end">
+                        <div class="recent-work-content text-start mb-3 ml-3 text-dark">
+                            <h3 class="card-title light-300">Kahmi Cita</h3>
+                            <p class="card-text">Psum officia anim id est laborum.</p>
+                        </div>
+                    </div>
+                </a>
+            </div><!-- End Recent Work -->
+
+            <!-- Start Recent Work -->
+            <div class="col-md-4 mb-3">
+                <a href="#" class="recent-work card border-0 shadow-lg overflow-hidden">
+                    <img class="recent-work-img card-img" src="{{ asset('frontend/img/kahmicita-open-donasi.jpeg') }}" alt="Card image">
+                    <div class="recent-work-vertical card-img-overlay d-flex align-items-end">
+                        <div class="recent-work-content text-start mb-3 ml-3 text-dark">
+                            <h3 class="card-title light-300">Kahmi Cita</h3>
+                            <p class="card-text">Sum dolor sit consencutur</p>
+                        </div>
+                    </div>
+                </a>
+            </div><!-- End Recent Work -->
+
+        </div>
+
+        <div class="row gy-5 g-lg-5 mb-4 justify-content-center">
+            <div class="col-8 col-md-4">
+                <a class="filter-btn nav-link btn-outline-primary active shadow rounded-pill text-light px-4 light-300 text-center" style="font-weight: bold; background-color: #fff; color: #0e5617 !important;" href="#" data-filter=".project">Lihat Semua Kegiatan</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Recent Work -->
+
+<section>
+    <div class="container my-5">
+        <div class="recent-work-header row pb-5 text-center">
+            <h2 class="col-md-6 m-auto h2 semi-bold-600 pt-2 typo-space-line-center-2" style="color: #184d47; font-size: 28px;">Desain Skematik Islamic Center</h2>
+        </div>
+        <div class="row">
+            <iframe width="679" height="480" src="https://www.youtube.com/embed/iOaJZp8wPn4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+</section>
+    
+@endsection
