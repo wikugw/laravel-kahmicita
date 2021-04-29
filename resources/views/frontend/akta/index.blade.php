8481ccf420448176cfae6e051b84d6f0c5a8a9ba
Wiku Galindra Wardhana <79816547+Wiku-Grune@users.noreply.github.com>
2021-04-29 20:42:37 +0700

    tambah halaman akta

---
+++ resources/views/frontend/akta/index.blade.php
@@ -0,0 +1,26 @@
+@extends('frontend.base.index')
+
+@section('title')
+<title>Akta | Yayasan KAHMI Cita</title>
+@endsection
+
+@section('content')
+    <!-- Start Service -->
+    <section class="service-wrapper py-3">
+        <div class="container-fluid pb-3">
+            <div class="row mt-4">
+                <div class="service-header col-2 col-lg-3 text-end light-300">
+                    <i class='bx bx-moon h3 mt-1'></i>
+                </div>
+                <div class="service-heading col-10 col-lg-9 text-start float-end light-300">
+                    <h2 class="h3 pb-4 typo-space-line">Akta Yayasan Kahmi</h2>
+                </div>
+            </div>
+            <p class="service-footer col-10 offset-2 col-lg-9 offset-lg-3 text-start pb-3 text-muted px-2">
+                <iframe src="https://drive.google.com/file/d/1ePmd8WFn5G5o-MsB0b4bjbQ1qgIkot-e/preview" width="640" height="480"></iframe>
+            </p>
+        </div>
+
+    </section>
+    <!-- End Service -->
+@endsection
